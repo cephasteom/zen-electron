e1b4e2021048ff9049a107b65d3bbb951a836d5f
Peter Thomas <petethomasmusique@gmail.com>
2025-03-25 16:26:04 +0000

    feature/save-load files

--- src/menus/fileMenu.js
+++ src/menus/fileMenu.js
@@ -1,4 +1,4 @@
-const { dialog } = require('electron');
+const { dialog, ipcMain } = require('electron');
 const Store = require('electron-store');
 const { serveSamples } = require('../samples/index.js');
 const fs = require('fs');
@@ -7,9 +7,40 @@ const isMac = process.platform === 'darwin'
 
 const store = new Store();
 
+store.set('currentFile', null);
+
 module.exports.generateFileMenu = ({
     mainWindow
 }) => {
+    const save = (filePath) => {
+        // ask mainwindow for current editor contents
+        console.log(filePath);
+        mainWindow.webContents.send('getCode');
+        ipcMain.once('getCodeResponse', (_, response) => {
+            // write to file
+            fs.writeFile(filePath,
+                response,
+                'utf8',
+                (err) => err
+                    ? console.error(`Error writing file: ${err}`)
+                    : console.log(`File is written successfully!`)
+            );
+
+            store.set('currentFile', filePath);
+        });
+    }
+
+    const saveAs = () => {
+        dialog.showSaveDialog({
+            title: 'Save File',
+            filters: [
+                { name: "JavaScript Files", extensions: ['js'] }
+            ]
+        }).then(({canceled, filePath}) => {
+            if (canceled) return;
+            save(filePath);
+        })
+    }
     
     return [{
         label: 'File',
@@ -33,9 +64,24 @@ module.exports.generateFileMenu = ({
                         if (canceled) return;
                         const file = fs.readFileSync(filePaths[0], 'utf8');
                         mainWindow.webContents.send('load', file);
+                        store.set('currentFile', filePaths[0]);
                     })
                 }
             },
+            { type: 'separator' },
+            {
+                label: 'Save',
+                accelerator: 'CmdOrCtrl+S',
+                click: () => store.get('currentFile')
+                    ? save(store.get('currentFile'))
+                    : saveAs()
+            },
+            {
+                label: 'Save As',
+                accelerator: 'CmdOrCtrl+Shift+S',
+                click: () => saveAs()
+            },
+            { type: 'separator' },
             { 
                 label: 'Load Samples Directory', 
                 click: () => {
--- src/preload.js
+++ src/preload.js
@@ -4,4 +4,6 @@ contextBridge.exposeInMainWorld('isApp', true)
 
 contextBridge.exposeInMainWorld('electronAPI', {
     onLoad: (callback) => ipcRenderer.on('load', (_, string) => callback(string)),
+    onGetCode: (callback) => ipcRenderer.on('getCode', () => callback()),
+    getCodeResponse: (string) => ipcRenderer.send('getCodeResponse', string),
 })
